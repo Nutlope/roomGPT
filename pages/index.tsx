a544ab88cf20a0e62a41ffe26e9bb96d03676d95
Hassan El Mghari <hassan4709@gmail.com>
2023-01-07 13:05:44 -0500

    UI v1, implemented upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,58 @@
-import type { NextPage } from "next";
+import { NextPage } from "next";
 import Head from "next/head";
+import { useState } from "react";
+import { UploadDropzone } from "react-uploader";
+import { Uploader } from "uploader";
+
+// Get production API keys from Upload.io
+const uploader = Uploader({
+  apiKey: "free",
+});
+
+// Customize the file upload UI (see "customization"):
+const options = { maxFileCount: 1, mimeTypes: ["image/jpeg", "image/png", "image/jpg"], editor: { images: { crop: false } } };
 
 const Home: NextPage = () => {
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+
+  const UploadDropZone = () => (
+    <UploadDropzone
+      uploader={uploader} // Required.
+      options={options} // Optional.
+      onUpdate={(file) => {
+        if (file.length === 0) {
+          console.log("No files selected.");
+        } else {
+          console.log("File selected: ", file[0].fileUrl);
+          setImageUrl(file[0].fileUrl);
+        }
+      }}
+      width="600px"
+      height="375px"
+    />
+  );
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen max-w-6xl mx-auto flex-col items-center justify-center py-2">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{" "}
-          <a className="text-blue-600" href="https://nextjs.org">
-            RestorePhotos.io!
-          </a>
+      <main className="flex w-full flex-1 flex-col items-center justify-center text-center px-20">
+        <h1 className="text-5xl sm:text-6xl font-bold mb-5">
+          Welcome to <span className="text-blue-600">Restore Photos!</span>
         </h1>
         {/* Input for a photo upload */}
-        <input type="file" name="photo" id="photo" />
+        <div className="flex justify-between items-center border w-full">
+          <UploadDropZone />
+          {imageUrl && (
+            <a href={imageUrl}>
+              {" "}
+              <img src={imageUrl} className="h-80" />
+            </a>
+          )}
+        </div>
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
