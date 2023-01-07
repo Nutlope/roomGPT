22a10a515edbda5d92eb343eccb65a4a2a59c60e
Hassan El Mghari <hassan4709@gmail.com>
2023-01-07 18:06:58 -0500

    added header ;

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # RestorePhotos.io
 
-A service that will restore old face photos.
+A site that restores people in old photos, specifically faces.
 
 ## Todos
 
 - [x] Get initial API route to work
 - [x] Figure how to feed API an image from the UI
 - [x] Create the UI for uploading an image
+- [x] Add header with an image icon, restorephotos.io, then a GitHub icon with the repo at the top right
+- [ ] Add some examples in some sort of sliding carousel at the end, or maybe add some near the upload
 - [ ] Migrate to S3 + filepond
 - [ ] Implement progress bar
-- [ ] Get an OG card
-- [ ] Add some examples in some sort of sliding carousel at the end, or maybe add some near the upload
 - [ ] Share page with dynamic OG card and a nav bar that just links back to the main site
-- [ ] Footer with GitHub and Twitter Logos with links to the repo and twitter
+- [ ] Get an OG card
+- [ ] Footer with links to Next.js, Vercel, and Replicate
+- [ ] Improve how it looks using Untitled UI – nice hero section
--- pages/index.tsx
+++ pages/index.tsx
@@ -55,16 +55,26 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div className="flex min-h-screen max-w-6xl mx-auto flex-col items-center justify-center py-2">
+    <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>Create Next App</title>
+        <title>Restore Photos</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center text-center sm:px-20 px-4">
+      <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-0 px-2">
+        <div className="flex space-x-2">
+          <img src="/picture.png" className="sm:w-14 sm:h-14 w-9 h-9" />
+          <h1 className="sm:text-5xl text-3xl font-bold ml-2 tracking-tight">restorePhotos.io</h1>
+        </div>
+        <img src="/github-mark.png" className="sm:w-10 sm:h-10 w-8 h-8" />
+      </header>
+      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-10">
         <h1 className="text-5xl sm:text-6xl font-bold mb-5">
-          Welcome to <span className="text-blue-600">Restore Photos!</span>
+          <span>
+            Restore <span className="">Old and Blurry</span> Photos!
+          </span>
         </h1>
+        <p className="max-w-2xl">Have old and blurry photos from your childhood? Let our AI restore them for you so those old memories can live on. Restore your old photos today – 100% free.</p>
         {/* Input for a photo upload */}
         <div className="flex justify-between items-center w-full flex-col">
           {!imageUrl && <UploadDropZone />}
@@ -76,10 +86,21 @@ const Home: NextPage = () => {
           )}
           {restoredImage && <img src={restoredImage} className="sm:h-80 h-60" />}
           {loading && <p>Loading...</p>}
+          <div className="flex flex-col space-y-10">
+            <h1 className="text-5xl">Examples:</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum unde distinctio nesciunt nostrum, omnis pariatur vero velit cupiditate facere! Inventore magnam sunt itaque quidem dignissimos ad nulla voluptatibus. Iusto, cumque.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum unde distinctio nesciunt nostrum, omnis pariatur vero velit cupiditate facere! Inventore magnam sunt itaque quidem dignissimos ad nulla voluptatibus. Iusto, cumque.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum unde distinctio nesciunt nostrum, omnis pariatur vero velit cupiditate facere! Inventore magnam sunt itaque quidem dignissimos ad nulla voluptatibus. Iusto, cumque.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum unde distinctio nesciunt nostrum, omnis pariatur vero velit cupiditate facere! Inventore magnam sunt itaque quidem dignissimos ad nulla voluptatibus. Iusto, cumque.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum unde distinctio nesciunt nostrum, omnis pariatur vero velit cupiditate facere! Inventore magnam sunt itaque quidem dignissimos ad nulla voluptatibus. Iusto, cumque.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum unde distinctio nesciunt nostrum, omnis pariatur vero velit cupiditate facere! Inventore magnam sunt itaque quidem dignissimos ad nulla voluptatibus. Iusto, cumque.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum unde distinctio nesciunt nostrum, omnis pariatur vero velit cupiditate facere! Inventore magnam sunt itaque quidem dignissimos ad nulla voluptatibus. Iusto, cumque.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum unde distinctio nesciunt nostrum, omnis pariatur vero velit cupiditate facere! Inventore magnam sunt itaque quidem dignissimos ad nulla voluptatibus. Iusto, cumque.</p>
+          </div>
         </div>
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
+      <footer className="flex h-24 w-full items-center justify-center border-t-2 mt-5">
         <a className="flex items-center justify-center gap-2" href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
           Powered by Next.js, Vercel, and Replicate.
         </a>
