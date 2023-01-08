f03797922d7876c127a9277bc849d0fd1a8d7308
Hassan El Mghari <hassan4709@gmail.com>
2023-01-07 23:10:05 -0500

    small refactoring, v1 ready

--- README.md
+++ README.md
@@ -2,16 +2,20 @@
 
 A site that restores people in old photos, specifically faces.
 
-## Todos
+## Todos v1
 
 - [x] Get initial API route to work
 - [x] Figure how to feed API an image from the UI
 - [x] Create the UI for uploading an image
 - [x] Add header with an image icon, restorephotos.io, then a GitHub icon with the repo at the top right
 - [x] Footer with links to Next.js, Vercel, and Replicate
-- [ ] Improve how hero section looks using Untitled UI / Tailwind UI
-- [ ] Add a new section from Tailwind/Untitled UI to show some examples
+- [x] Improve how hero section looks using Untitled UI / Tailwind UI
+- [x] Add a new section from Tailwind/Untitled UI to show some examples
+
+## Todos v2
+
 - [ ] Migrate to S3 + filepond
 - [ ] Implement progress bar
-- [ ] Share page with dynamic OG card and a nav bar that just links back to the main site
-- [ ] Get an OG card
+- [ ] Implement share page with dynamic OG card and link to header to go back to main page
+- [ ] Add an OG card
+- [ ] Write out README, add to Templates Marketplace
--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ const Home: NextPage = () => {
           generatePhoto(file[0].fileUrl);
         }
       }}
-      width="500px"
+      width="600px"
       height="200px"
     />
   );
@@ -61,25 +61,37 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-0 px-2">
+      <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
         <div className="flex space-x-2">
-          <img src="/picture.png" className="sm:w-14 sm:h-14 w-9 h-9" />
+          <img src="/imageIcon.png" className="sm:w-14 sm:h-14 w-9 h-9" />
           <h1 className="sm:text-5xl text-3xl font-bold ml-2 tracking-tight">restorePhotos.io</h1>
         </div>
         <a href="https://github.com/Nutlope/whatisdevrel" target="_blank" rel="noreferrer">
-          <img src="/github-mark.png" className="sm:w-10 sm:h-10 w-8 h-8" />
+          <img src="/githubIcon.png" className="sm:w-10 sm:h-10 w-8 h-8" />
         </a>
       </header>
-      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-10">
-        <h1 className="text-5xl sm:text-6xl font-bold mb-5">
-          <span>
-            Restore <span className="">Old and Blurry</span> Photos!
-          </span>
+      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-28 mt-20">
+        <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-slate-900 sm:text-7xl">
+          Restoring old photos{" "}
+          <span className="relative whitespace-nowrap text-blue-600">
+            <svg aria-hidden="true" viewBox="0 0 418 42" className="absolute top-2/3 left-0 h-[0.58em] w-full fill-blue-300/70" preserveAspectRatio="none">
+              <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z" />
+            </svg>
+            <span className="relative">using AI</span>
+          </span>{" "}
+          for everyone.
         </h1>
-        <p className="max-w-2xl">Have old and blurry photos from your childhood? Let our AI restore them for you so those old memories can live on. Restore your old photos today – 100% free.</p>
-        {/* Input for a photo upload */}
-        <div className="flex justify-between items-center w-full flex-col">
-          {!imageUrl && <UploadDropZone />}
+        <p className="mx-auto mt-12 max-w-xl text-lg text-slate-700 leading-7">Have old and blurry photos? Let our AI restore them so those memories can live on. Restore your old photos today – 100% free.</p>
+        <button className="bg-black rounded-full text-white font-medium px-4 py-2 mt-8 hover:bg-black/80">Restore your photos</button>
+        <div className="flex justify-between items-center w-full flex-col mt-6">
+          <div className="flex flex-col space-y-10 mt-4">
+            <div className="flex sm:space-x-2 sm:flex-row flex-col">
+              <img src="originalLilBro.png" className="w-80 rounded-2xl" />
+              <img src="restoredLilBro.png" className="w-80 rounded-2xl sm:mt-0 mt-2" />
+            </div>
+          </div>
+          <p className="text-gray-400 mt-2">Check out this sweet photo restoration of my little brother.</p>
+          {/* {!imageUrl && <UploadDropZone />}
           {imageUrl && <img src={imageUrl} className="sm:h-80 h-60" />}
           {imageUrl && (
             <button onClick={() => setImageUrl(null)} className="bg-blue-500 rounded-xl p-3 mt-5 mb-2">
@@ -87,18 +99,7 @@ const Home: NextPage = () => {
             </button>
           )}
           {restoredImage && <img src={restoredImage} className="sm:h-80 h-60" />}
-          {loading && <p>Loading...</p>}
-          <div className="flex flex-col space-y-10">
-            <h1 className="text-5xl">Examples:</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum unde distinctio nesciunt nostrum, omnis pariatur vero velit cupiditate facere! Inventore magnam sunt itaque quidem dignissimos ad nulla voluptatibus. Iusto, cumque.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum unde distinctio nesciunt nostrum, omnis pariatur vero velit cupiditate facere! Inventore magnam sunt itaque quidem dignissimos ad nulla voluptatibus. Iusto, cumque.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum unde distinctio nesciunt nostrum, omnis pariatur vero velit cupiditate facere! Inventore magnam sunt itaque quidem dignissimos ad nulla voluptatibus. Iusto, cumque.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum unde distinctio nesciunt nostrum, omnis pariatur vero velit cupiditate facere! Inventore magnam sunt itaque quidem dignissimos ad nulla voluptatibus. Iusto, cumque.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum unde distinctio nesciunt nostrum, omnis pariatur vero velit cupiditate facere! Inventore magnam sunt itaque quidem dignissimos ad nulla voluptatibus. Iusto, cumque.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum unde distinctio nesciunt nostrum, omnis pariatur vero velit cupiditate facere! Inventore magnam sunt itaque quidem dignissimos ad nulla voluptatibus. Iusto, cumque.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum unde distinctio nesciunt nostrum, omnis pariatur vero velit cupiditate facere! Inventore magnam sunt itaque quidem dignissimos ad nulla voluptatibus. Iusto, cumque.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum unde distinctio nesciunt nostrum, omnis pariatur vero velit cupiditate facere! Inventore magnam sunt itaque quidem dignissimos ad nulla voluptatibus. Iusto, cumque.</p>
-          </div>
+          {loading && <p>Loading...</p>} */}
         </div>
       </main>
 
