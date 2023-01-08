b639e7b07142c9d2da6acedbd712574bc68ac1fe
Hassan El Mghari <hassan4709@gmail.com>
2023-01-08 18:11:27 -0500

    improve accessibility and SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,11 +31,11 @@ const Home: NextPage = () => {
           <div className="flex flex-col space-y-10 mt-4">
             <div className="flex sm:space-x-2 sm:flex-row flex-col">
               <div>
-                <h3 className="mb-1 font-medium text-lg">Original Photo</h3>
+                <h2 className="mb-1 font-medium text-lg">Original Photo</h2>
                 <Image alt="Original photo of my bro" src="/originalbro.png" className="w-80 rounded-2xl" width={300} height={300} />
               </div>
               <div className="sm:mt-0 mt-8">
-                <h3 className="mb-1 font-medium text-lg">Restored Photo</h3>
+                <h2 className="mb-1 font-medium text-lg">Restored Photo</h2>
                 <Image alt="Restored photo of my bro" width={300} height={300} src="/restoredbro.png" className="w-80 rounded-2xl sm:mt-0 mt-2" />
               </div>
             </div>
