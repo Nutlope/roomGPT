eaf6dec8e88368c63616c9b7e91b106e35294397
Hassan El Mghari <hassan4709@gmail.com>
2023-01-12 14:15:49 -0500

    added link to replicate in the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,15 +4,34 @@ export default function Footer() {
   return (
     <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
       <div>
+        Powered by{" "}
         <a
-          href="https://nutlope.substack.com/"
+          href="https://nextjs.org/"
           target="_blank"
           rel="noreferrer"
           className="font-bold hover:underline transition underline-offset-2"
         >
-          Subscribe to my newsletter
-        </a>{" "}
-        to hear about new updates and my latest side projects.
+          Next.js
+        </a>
+        ,{" "}
+        <a
+          href="https://vercel.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="font-bold hover:underline transition underline-offset-2"
+        >
+          Vercel
+        </a>
+        , &{" "}
+        <a
+          href="https://replicate.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="font-bold hover:underline transition underline-offset-2"
+        >
+          Replicate
+        </a>
+        .
       </div>
       <div className="flex space-x-4 pb-4 sm:pb-0">
         <Link
