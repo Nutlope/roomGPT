d8e26db17dff42fd906f498026d3900ee31ca2dc
Hassan El Mghari <hassan4709@gmail.com>
2023-03-20 14:51:22 -0400

    Update dashboard

--- components/RoomGenerator.tsx
+++ components/RoomGenerator.tsx
@@ -8,14 +8,14 @@ export function RoomGeneration({
   generated: string;
 }) {
   return (
-    <div className="flex flex-col space-y-10 mt-4 mb-4 border p-8 border-gray-600 rounded-xl">
-      <div className="flex sm:space-x-8 sm:flex-row flex-col">
+    <div className="flex flex-col space-y-10 mt-4 mb-4 border px-8 pb-8 pt-2 border-gray-600 rounded-xl">
+      <div className="flex sm:space-x-8 sm:flex-row flex-col pb-5">
         <div>
           <h3 className="mb-1 font-medium text-lg">Original</h3>
           <Image
             alt="Original room"
             src={original}
-            className="w-full rounded-2xl aspect-auto"
+            className="rounded-2xl h-full"
             width={400}
             height={400}
           />
@@ -27,7 +27,7 @@ export function RoomGeneration({
             width={400}
             height={400}
             src={generated}
-            className="w-full rounded-2xl sm:mt-0 mt-2 aspect-auto"
+            className="rounded-2xl h-full sm:mt-0 mt-2"
           />
         </div>
       </div>
