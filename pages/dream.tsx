298af65c669dae33df5b21559336aa5967bfba52
Hassan El Mghari <hassan4709@gmail.com>
2023-03-23 14:17:24 -0400

    added tags to upload to know if free or paid user

--- pages/dream.tsx
+++ pages/dream.tsx
@@ -49,6 +49,7 @@ const Home: NextPage = () => {
     maxFileCount: 1,
     mimeTypes: ["image/jpeg", "image/png", "image/jpg"],
     editor: { images: { crop: false } },
+    tags: [data.remainingGenerations > 5 ? "paid" : "free"],
     styles: {
       colors: {
         primary: "#2563EB", // Primary buttons & links
