77a94066bebc48a4413d0605d55677f4fc325331
Charles <chkya8@gmail.com>
2023-04-02 00:21:46 +0800

    canvas compatibility

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
 module.exports = {
   reactStrictMode: true,
   images: {
