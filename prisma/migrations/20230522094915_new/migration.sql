b0fd08fa08694fd5a89458aa430966e08603a2af
Charles <chkya8@gmail.com>
2023-05-22 17:50:02 +0800

    migration

---
+++ prisma/migrations/20230522094915_new/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - Added the required column `paymentAmount` to the `Purchase` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Purchase" ADD COLUMN     "paymentAmount" INTEGER NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "plan" TEXT NOT NULL DEFAULT 'basic';
