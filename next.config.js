38146fe6aa9d37963f97bd67582681c6ed28f8ba
Hassan El Mghari <hassan4709@gmail.com>
2023-06-01 17:05:06 -0400

    more cleanup

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  experimental: {
-    serverActions: true,
-  },
   reactStrictMode: true,
   images: {
     domains: ["upcdn.io", "replicate.delivery"],
