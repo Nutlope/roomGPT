2da65c5628411747ab3ca85a04d4025ecb99aba1
Ala Eddine <ala.official.acc@gmail.com>
2023-06-18 11:23:30 +0100

    :cyclone: Defined type for props

--- components/LoadingDots.tsx
+++ components/LoadingDots.tsx
@@ -1,23 +1,22 @@
-import styles from "../styles/loading-dots.module.css";
+import styles from '../styles/loading-dots.module.css';
 
-const LoadingDots = ({
-  color = "#000",
-  style = "small",
-}: {
-  color: string;
-  style: string;
-}) => {
-  return (
-    <span className={style == "small" ? styles.loading2 : styles.loading}>
-      <span style={{ backgroundColor: color }} />
-      <span style={{ backgroundColor: color }} />
-      <span style={{ backgroundColor: color }} />
-    </span>
-  );
+interface IProps {
+	color: string;
+	style: string;
+}
+
+const LoadingDots = ({ color = '#000', style = 'small' }: IProps) => {
+	return (
+		<span className={style == 'small' ? styles.loading2 : styles.loading}>
+			<span style={{ backgroundColor: color }} />
+			<span style={{ backgroundColor: color }} />
+			<span style={{ backgroundColor: color }} />
+		</span>
+	);
 };
 
 export default LoadingDots;
 
 LoadingDots.defaultProps = {
-  style: "small",
+	style: 'small',
 };
